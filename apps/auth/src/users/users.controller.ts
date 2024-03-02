5da1974362cc3e57873b08aa7a967f759389a0d0
Dmitry Inke <angranity1998@gmail.com>
2024-03-02 19:44:38 +0200

    Protected routes in the microservice for reservations with JwtAuthGuard, connecting microservices together using the TCP transport layer.

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
-import { CurrentUser } from '../current-user.decorator';
 import { UserDocument } from './models/user.schema';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { CurrentUser } from '@app/common';
 
 @Controller('users')
 export class UsersController {
@@ -14,8 +14,8 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
   @UseGuards(JwtAuthGuard)
+  @Get()
   async getUser(@CurrentUser() user: UserDocument) {
     return user;
   }
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import * as coockieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
+  app.use(coockieParser());
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
