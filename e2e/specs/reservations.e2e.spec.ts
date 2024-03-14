03559b07e40fe87869ae0304ed4334470aa4e9e0
Dmitry Inke <angranity1998@gmail.com>
2024-03-14 18:35:53 +0200

    e2e tests done

---
+++ e2e/specs/reservations.e2e.spec.ts
@@ -0,0 +1,69 @@
+describe('Reservations', () => {
+  let jwt: string;
+
+  beforeAll(async () => {
+    const user = {
+      email: 'angranity1998@gmail.com',
+      password: '123456blablaS!',
+    };
+    await fetch('http://auth:3001/users', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const response = await fetch('http://auth:3001/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    jwt = await response.text();
+  });
+
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          Authentication: jwt,
+        },
+      },
+    );
+    const reservation = await responseGet.json();
+    expect(createdReservation).toEqual(reservation);
+  });
+
+  const createReservation = async () => {
+    const responseCreate = await fetch(
+      'http://reservations:3000/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+        body: JSON.stringify({
+          startDate: '02/27/2024',
+          endDate: '03/03/2024',
+          placeId: '12345',
+          invoiceId: '493',
+          charge: {
+            amount: 123222,
+            card: {
+              cvc: '413',
+              exp_month: 12,
+              exp_year: 2027,
+              number: '4242 4242 4242 4242',
+            },
+          },
+        }),
+      },
+    );
+    expect(responseCreate.ok).toBeTruthy();
+    return responseCreate.json();
+  };
+});
